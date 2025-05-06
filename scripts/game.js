fa94d80642e3f1ef36167a669cc976bc2d7a3a91
zaedian <55536305+zaedian@users.noreply.github.com>
2025-05-06 23:35:41 +0200

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -316,10 +316,10 @@ for (let i = 0; i < vertices.length; i += 3) {
             });
 			
 			
-			const neonLight = new THREE.RectAreaLight(0x0000ff, 8, 2, 4.2);
+			/*const neonLight = new THREE.RectAreaLight(0x0000ff, 6, 2, 4.2);
 			neonLight.position.set(0, -0.5, 0);
 			neonLight.rotation.x = -Math.PI / 2;
-			chassisMesh.add(neonLight);
+			chassisMesh.add(neonLight);*/
 
 
             scene.add(chassisMesh);
